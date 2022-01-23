d88c11a71f64c48a616439020b26a9db7788ccc5
Learn-about-coding <83706533+Samar123455@users.noreply.github.com>
2022-01-23 18:31:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 if(screen.width<740){
-    document.getElementById("intro-image").innerHTML = "<img id='intro-image' src='file:///D:/samar%20resources/Notes%20for%20whitehat/My%20Independant%20websites/What%20is%20coding/Image-3.jpg' >";
+    document.getElementById("intro-image").value = "https://www.lifeintech.com/images/posts/kidscoding01.jpg"
 }
